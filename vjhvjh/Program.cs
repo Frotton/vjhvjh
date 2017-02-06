d9505cd3c6d2d6c7100cb9963b5f0aa1d1ccd6c9
201582715 <201582715@P106-10>
2017-02-06 08:35:50 -0500

    J'ai dit bonjour à Jean.

--- vjhvjh/Program.cs
+++ vjhvjh/Program.cs
@@ -11,6 +11,7 @@ namespace vjhvjh
         static void Main(string[] args)
         {
             Console.WriteLine("Bonjour Paul");
+            Console.WriteLine("Bonjour Jean");
         }
     }
 }
